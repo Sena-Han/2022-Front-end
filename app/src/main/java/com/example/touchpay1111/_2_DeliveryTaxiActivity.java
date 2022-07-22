79941fa764d8b8052bee972cce57a72ea4952cbd
Han seyoung <88265447+seyoung-Han-ouo@users.noreply.github.com>
2022-07-22 21:17:41 +0900

    commit

--- app/src/main/java/com/example/touchpay1111/_2_DeliveryTaxiActivity.java
+++ app/src/main/java/com/example/touchpay1111/_2_DeliveryTaxiActivity.java
@@ -2,13 +2,30 @@ package com.example.touchpay1111;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class _2_DeliveryTaxiActivity extends AppCompatActivity {
+
+    Button deliveryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2_delivery_taxi);
+
+        deliveryButton = findViewById(R.id.deliveryButton);
+
+        deliveryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), _3_D_MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
