22b3bd31e727ae17484690c02064fef17c4b262b
ssohy <sohyeonj415@gmail.com>
2022-07-22 18:31:15 +0900

    git commit

--- app/src/main/java/com/example/touchpay1111/_4_T_s1Activity.java
+++ app/src/main/java/com/example/touchpay1111/_4_T_s1Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class _4_T_StationSelect_2Activity extends AppCompatActivity {
+public class _4_T_s1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_4_tstation_select2);
+        setContentView(R.layout.activity_4_ts1);
     }
 }
