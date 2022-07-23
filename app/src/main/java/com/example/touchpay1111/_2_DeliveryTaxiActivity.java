ddac8b4205a9ad61f837d60c8d9143c6518986cd
Sena-Han <hsysn1@naver.com>
2022-07-23 21:15:00 +0900

    commit

--- app/src/main/java/com/example/touchpay1111/_2_DeliveryTaxiActivity.java
+++ app/src/main/java/com/example/touchpay1111/_2_DeliveryTaxiActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 public class _2_DeliveryTaxiActivity extends AppCompatActivity {
 
-    Button deliveryButton;
+    Button deliveryButton, taxiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +18,19 @@ public class _2_DeliveryTaxiActivity extends AppCompatActivity {
         setContentView(R.layout.activity_2_delivery_taxi);
 
         deliveryButton = findViewById(R.id.deliveryButton);
+        taxiButton = findViewById(R.id.taxiButton);
 
         deliveryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), _3_D_MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+        taxiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), _4_T_SchoolSelectActivity.class);
                 startActivity(intent);
             }
         });
