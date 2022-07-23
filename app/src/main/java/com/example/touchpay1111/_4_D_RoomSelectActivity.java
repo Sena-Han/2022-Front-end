f0bd24f41ea3eadd7d39716bc77a068140c7a2e6
Sena-Han <hsysn1@naver.com>
2022-07-23 20:38:54 +0900

    commit

--- app/src/main/java/com/example/touchpay1111/_4_D_RoomSelectActivity.java
+++ app/src/main/java/com/example/touchpay1111/_4_D_RoomSelectActivity.java
@@ -2,13 +2,28 @@ package com.example.touchpay1111;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class _4_D_RoomSelectActivity extends AppCompatActivity {
+
+    Button newRoomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_droom_select);
+
+        newRoomButton = findViewById(R.id.newRoomButton);
+
+        newRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), _5_D_NewRoomActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
