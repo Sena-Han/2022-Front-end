378d7edc760b5f102909d2e380c4a41f9e3ac6a8
Sena-Han <hsysn1@naver.com>
2022-07-26 21:23:27 +0900

    commit

--- app/src/main/java/com/example/touchpay1111/_4_T_SchoolSelectActivity.java
+++ app/src/main/java/com/example/touchpay1111/_4_T_SchoolSelectActivity.java
@@ -2,7 +2,10 @@ package com.example.touchpay1111;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class _4_T_SchoolSelectActivity extends AppCompatActivity {
 
