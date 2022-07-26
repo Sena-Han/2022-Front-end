df2c9e5d5b26bdfccec7d1ff06d6f2ebed259bb8
Sena-Han <hsysn1@naver.com>
2022-07-26 23:37:26 +0900

    commit

---
+++ app/src/main/java/com/example/touchpay1111/_4_T_s2Activity.java
@@ -0,0 +1,15 @@
+package com.example.touchpay1111;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class _4_T_s2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4_ts2);
+    }
+}
