2fa26b77192ba377e46194c19dafd16252ff311e
ssohy <sohyeonj415@gmail.com>
2022-07-28 22:57:50 +0900

    git commit

--- app/src/main/java/com/example/touchpay1111/_4_T_s1Activity.java
+++ app/src/main/java/com/example/touchpay1111/_4_T_s1Activity.java
@@ -7,12 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class _4_T_s1Activity extends AppCompatActivity {
+    TextView Hwaseo=(TextView)findViewById(R.id.Hwaseo);
+    TextView Seryu=(TextView)findViewById(R.id.Seryu);
+    TextView Seodongtan=(TextView)findViewById(R.id.Seodongtan);
+    TextView Byeongjeom=(TextView)findViewById(R.id.Byeongjeom);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts1);
+
+
+
+
     }
 }
