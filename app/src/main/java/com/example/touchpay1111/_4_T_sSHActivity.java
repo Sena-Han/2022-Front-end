19051a69291418d6f0a56545d3904507b21b75bb
ssohy <sohyeonj415@gmail.com>
2022-07-29 22:26:35 +0900

    git commit

---
+++ app/src/main/java/com/example/touchpay1111/_4_T_sSHActivity.java
@@ -0,0 +1,14 @@
+package com.example.touchpay1111;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _4_T_sSHActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4_ts_shactivity);
+    }
+}
